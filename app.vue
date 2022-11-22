d99079db7937f7d3214cfe8c8c2772a8e911a96b
Woody Chang <woody.chang@storm.mg>
2022-11-22 18:03:36 +0800

    Add eslint

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <Hello />
+  </div>
+</template>
---
+++ components/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    HELLO
+  </div>
+</template>
+<script setup lang="ts">
+const year: number = 2022
+const title: string = `${year} iThome 鐵人賽`
+</script>
+<style lang="">
+  
+</style>
