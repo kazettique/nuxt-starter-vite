a4598f061fed06856e8487b7a15327f6c16176e7
Woody Chang <woody.chang@storm.mg>
2022-11-23 11:03:20 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,12 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [],
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'vue/multi-word-component-names': 'warn'
+  },
   overrides: [
     {
       files: ['**/pages/**/*.{js,ts,vue}', '**/layouts/**/*.{js,ts,vue}', '**/app.{js,ts,vue}', '**/error.{js,ts,vue}'],
